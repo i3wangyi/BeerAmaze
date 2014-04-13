bfff3cd43dc6894998f696ba006092a7200a4d40
i3wangyi <i3.wangyi@gmail.com>
2014-04-13 01:23:36 -0400

    Add GUI and Selection, Rotation script

---
+++ Assets/Script/Characters/ObjectBehavior.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBehavior : MonoBehaviour {
+
+	public bool isPlayMode = true;
+	public bool EnableTranslate = false;
+	public bool EnableRotate = false;
+
+	public GameObject MainCamera;
+	public Vector3 CameraPrevPos;
+	public Vector3 CameraPrevOri;
+	public Vector3 CameraNewPos;
+	public Vector3 CameraNewOri;
+	//Object position and orientation
+	public Vector3 prevPos;
+	public Vector3 PrevOri;
+
+	public Vector3 newPos;
+	public Vector3 newOri;
+
+	// Use this for initialization
+	void Start () {
+		MainCamera = GameObject.Find ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (isPlayMode) {
+
+			CameraNewPos = MainCamera.transform.position;
+			CameraNewOri = MainCamera.transform.eulerAngles;
+
+			Vector3 CameraPosDiff = CameraNewPos - CameraPrevPos;
+			Vector3 CameraOriDiff = CameraNewOri - CameraPrevOri;
+
+
+			if(EnableTranslate)
+			{
+				//Control move in the plane
+				transform.position += new Vector3(CameraPosDiff.x,0,CameraPosDiff.z);
+			}
+			if(EnableRotate)
+			{
+				//Add: HeadLight behavior
+				transform.eulerAngles -= new Vector3(CameraOriDiff.x,0,CameraOriDiff.z);
+			}
+
+			CameraPrevPos = CameraNewPos;
+			CameraPrevOri = CameraNewOri;
+
+		}
+	}
+
+	public void setPlayMode()
+	{
+		isPlayMode = true;
+	}
+	public void resetPlayMode()
+	{
+		isPlayMode = false;
+	}
+	public void setMove()
+	{
+		EnableTranslate = true;
+	}
+	public void resetMove()
+	{
+		EnableTranslate = false;
+	}
+	public void setRotate()
+	{
+		EnableRotate = true;
+	}
+	public void resetRotate()
+	{
+		EnableRotate = false;
+	}
+
+	//Called when user hold the UI's translate or rotate button, used to record the camera's position at that time
+	public void setCameraStatus()
+	{
+		CameraPrevPos = MainCamera.transform.position;
+		CameraPrevOri = MainCamera.transform.eulerAngles;
+	}
+	                 
+}
---
+++ Assets/Script/Mobile_GUI/UI.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+	//Indicate which play mode at present
+	public bool isPlayMode = true;
+	public GameObject MainObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//Set mobile GUI
+	void onGUI()
+	{
+		if(isPlayMode)
+		{
+			GUILayout.BeginArea(new Rect(0,0,400,1000) );
+			GUILayout.BeginVertical("box");
+			GUILayout.BeginHorizontal("box");
+			//Translate
+			if(GUILayout.RepeatButton("Move"))
+			{
+				MainObject.GetComponent<ObjectBehavior>().setMove();
+				MainObject.GetComponent<ObjectBehavior>().setCameraStatus();
+			}
+			else{
+				MainObject.GetComponent<ObjectBehavior>().resetMove();
+			}
+			GUILayout.EndHorizontal();
+
+
+			GUILayout.BeginHorizontal("box");
+			//Rotate
+			if(GUILayout.RepeatButton("Look around"))
+			{
+				MainObject.GetComponent<ObjectBehavior>().setRotate();
+				MainObject.GetComponent<ObjectBehavior>().setCameraStatus();
+			}
+			else{
+				MainObject.GetComponent<ObjectBehavior>().resetRotate();
+			}
+			GUILayout.EndHorizontal();
+		}
+	}
+	//When given object is selected, its character will be passed to the UI
+	public void setCharacter(GameObject Character)
+	{
+		MainObject = Character;
+	}
+	public void setPlayMode()
+	{
+		isPlayMode = true;
+	}
+	public void resetPlayMode()
+	{
+		isPlayMode = false;
+	}
+}
