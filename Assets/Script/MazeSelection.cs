3acb3f7dddd6326e87b0ede54f5711d568b589a8
Shuai Lu <sl3635@columbia.edu>
2014-04-30 00:41:35 -0700

    welcome menu

---
+++ Assets/Script/MazeSelection.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeSelection : MonoBehaviour {
+	private GameObject[] objArray;
+	private Vector3[] startOri;
+	private int it;
+	private Vector2 startPos;
+	private Vector2 endPos;
+	private GameObject currentObj;
+	private float countDown;
+	// Use this for initialization
+	void Start () {
+		objArray = new GameObject[4];
+		startOri = new Vector3[4];
+		objArray [0] = GameObject.Find ("Capsule");
+		objArray [1] = GameObject.Find ("Cube");
+		objArray [2] = GameObject.Find ("Cylinder");
+		objArray [3] = GameObject.Find ("Sphere");
+		objArray [0].SetActive(true);
+		objArray [1].SetActive(false);
+		objArray [2].SetActive(false);
+		objArray [3].SetActive(false);
+		for (int i = 0; i < 4; i++) {
+			startOri[i] = objArray[i].transform.eulerAngles;
+				}
+		it = 0;
+		currentObj = objArray [0];
+		countDown = 300.0f;
+
+	}
+	
+	// Update is called once per frame
+	void OnGUI(){
+		countDown -= Time.deltaTime;
+		GUI.skin.GetStyle ("Label").fontSize = 45;
+		GUILayout.BeginArea (new Rect (Screen.width/2 - 300, Screen.height/2 - 500, 600, 1000));
+		GUILayout.BeginVertical ("box");
+		GUILayout.Label (countDown.ToString ());
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+		}
+	void Update () {
+			for (int i = 0; i < Input.touchCount; i++) {
+			if(Input.GetTouch(i).phase == TouchPhase.Began)
+			{
+				startPos = Input.GetTouch(i).position;
+			}
+			if(Input.GetTouch(i).phase == TouchPhase.Ended)
+			{
+				endPos = Input.GetTouch(i).position;
+				if(endPos.x - startPos.x > 0)
+				{
+					objArray[it].SetActive(false);
+					it = (it + 1) % 4;
+					objArray[it].SetActive(true);
+					currentObj = objArray[it];
+					currentObj.transform.eulerAngles = startOri[it];
+				}
+				if(endPos.x - startPos.x < 0)
+				{
+					objArray[it].SetActive(false);
+					if(it == 0)
+						it = 3;
+					else
+						it --;
+					objArray[it].SetActive(true);
+					currentObj = objArray[it];
+					currentObj.transform.eulerAngles = startOri[it];
+				}
+			}
+		}
+		currentObj.transform.Rotate (0, 10 * Time.deltaTime, 0);
+	}
+}
---
+++ Assets/Script/StartMenuScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuScript : MonoBehaviour 
+{
+
+	private static bool HelpSelected = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		//For Mobile Debugging
+		GUI.skin.GetStyle ("Button").fontSize = 45;	
+		GUI.skin.GetStyle ("TextField").fontSize = 45;	
+		GUI.skin.GetStyle ("box").fontSize = 45;
+
+		GUILayout.BeginArea (new Rect (Screen.width/2 - 300, Screen.height/2 - 500, 600, 1000));
+		GUILayout.BeginVertical ("box");
+
+		if (HelpSelected == false) 
+		{
+
+			//Start
+			if (GUILayout.Button ("START")) 
+			{
+				Application.LoadLevel ("MazeSelection");
+			}
+			//Help
+			if (GUILayout.Button ("HELP"))
+			{
+				HelpSelected = true;
+			}
+
+		}
+		else 
+		{
+
+
+			GUILayout.TextField("Welcome to OurMazeGame!!");
+			if (GUILayout.Button ("CLOSE"))
+			{
+				HelpSelected = false;
+			}
+		}
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+	}
+}
