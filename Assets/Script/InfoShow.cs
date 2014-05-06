83e4819aa8783649b243b49401b04659c09198ee
Shuai Lu <sl3635@columbia.edu>
2014-05-05 23:22:14 -0400

    UI Optimized

---
+++ Assets/Script/InfoShow.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfoShow : MonoBehaviour {
+	public int count = 0;
+	public Texture[] infoImage = new Texture[3];
+	public Texture nextTex;
+	public Texture preTex;
+	public bool info = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI(){
+		if(info)
+		{
+			GUI.backgroundColor = Color.clear;
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), infoImage[count]);
+			GUILayout.BeginArea(new Rect (Screen.width -550, Screen.height - 250, 550, 250));
+			GUILayout.BeginHorizontal("box");
+			if (GUILayout.Button (preTex)) {
+				if(count > 0)
+					count--;
+			}
+			if (GUILayout.Button (nextTex)) {
+				count++;
+				if(count > 2)
+				{
+					info = false;
+					count = 0;
+					GameObject.Find("Camera").GetComponent<StartMenuScript>().startMenu = true;
+				}
+			}
+			GUILayout.EndHorizontal ();
+			GUILayout.EndArea ();
+		}
+	}
+}
--- Assets/Script/StartMenuScript.cs
+++ Assets/Script/StartMenuScript.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class StartMenuScript : MonoBehaviour 
 {
-
-	private static bool HelpSelected = false;
-
+	public bool startMenu = true;
+	public  bool HelpSelected = false;
+	public  bool AboutUsSelected = false;
+	public Texture backgroundTexture;
 	// Use this for initialization
 	void Start () 
 	{
-	
 	}
 	
 	// Update is called once per frame
@@ -20,40 +20,47 @@ public class StartMenuScript : MonoBehaviour
 
 	void OnGUI()
 	{
-		//For Mobile Debugging
-		GUI.skin.GetStyle ("Button").fontSize = 45;	
-		GUI.skin.GetStyle ("TextField").fontSize = 45;	
-		GUI.skin.GetStyle ("box").fontSize = 45;
-
-		GUILayout.BeginArea (new Rect (Screen.width/2 - 300, Screen.height/2 - 500, 600, 1000));
-		GUILayout.BeginVertical ("box");
-
-		if (HelpSelected == false) 
+		if (startMenu)
 		{
+			//For Mobile Debugging
+			GUI.skin.GetStyle ("Button").fontSize = 45;	
+			GUI.skin.GetStyle ("TextField").fontSize = 45;	
+			GUI.skin.GetStyle ("box").fontSize = 45;
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+			GUILayout.BeginArea (new Rect (Screen.width / 2 - 300, Screen.height / 2 - 500, 600, 1000));
+			GUILayout.BeginVertical ("box");
 
-			//Start
-			if (GUILayout.Button ("START")) 
-			{
-				Application.LoadLevel ("MazeSelection");
+			if (!AboutUsSelected && !HelpSelected) {
+
+					//Start
+					if (GUILayout.Button ("Start!")) {
+							Application.LoadLevel ("MazeSelection");
+					}
+					//Help
+					if (GUILayout.Button ("Help")) {
+							HelpSelected = true;
+					}
+					//Help
+					if (GUILayout.Button ("AboutUs")) {
+						AboutUsSelected = true;
+					}
+
 			}
-			//Help
-			if (GUILayout.Button ("HELP"))
+			if(HelpSelected)
 			{
-				HelpSelected = true;
-			}
-
-		}
-		else 
-		{
-
-
-			GUILayout.TextField("Welcome to OurMazeGame!!");
-			if (GUILayout.Button ("CLOSE"))
-			{
+				startMenu = false;
 				HelpSelected = false;
+				GameObject.Find("Camera").GetComponent<InfoShow>().info = true;
 			}
+			if(AboutUsSelected)
+			{
+				GUILayout.TextField ("Welcome to OurMazeGame!!");
+				if (GUILayout.Button ("CLOSE")) {
+					AboutUsSelected = false;
+				}
+			}
+			GUILayout.EndVertical ();
+			GUILayout.EndArea ();
 		}
-		GUILayout.EndVertical ();
-		GUILayout.EndArea ();
 	}
 }
